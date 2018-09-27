fb6b7b771ccf27712294540e399fb1ff956d73ca
samdcoder <pathan.sameer3@gmail.com>
2018-09-27 17:23:19 +0530

    user list displaying on /contacts route

--- public/fetch_contacts.js
+++ public/fetch_contacts.js
@@ -1,16 +1,19 @@
 $(document).ready(function(){
-    alert("in the ready function!");
+    
 	  $.ajax({
             url: "/getJson",
             type: 'GET',
             success: function (data) {
-                console.log(data);
+                
                 var result = $('#contact-list');
                 var counter = 1;
-               /*for(var key in data){
-               	result.append('<tr> <th scope="row">'+ counter + '</th>' + '<td>' + data[key].name+'</td>'+ '<td>'+data[key].email+'</td>'+'<td>'+data[key].phone+'</td>'+'<td>'+data[key].job+'</td>'+'</tr>');
+                data = data['contacts'];
+               for(var key in data){
+                console.log("Key = ", key);
+               	result.append('<h3> '+ counter + ": " + data[key].firstName + '  ' + data[key].lastName + '<br>');
                 counter++;
-               }*/
+               }
+
                result.append(data);
                
             },
