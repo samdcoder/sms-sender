5607cfb911aeefc99970589f6a26429b6aa3e1af
samdcoder <pathan.sameer3@gmail.com>
2018-09-29 01:56:26 +0530

    changed jumbotron color; used cards for contacts instead of list-items; added new dummy contacts

--- public/js/fetch_contacts.js
+++ public/js/fetch_contacts.js
@@ -6,12 +6,18 @@ $(document).ready(function(){
                 var result = $('#contact-list');
                 data = data['contacts'];
                 result.append('<div class="card" style="width: 18rem;">')
-                result.append('<ul class="list-group">')
                for(var key in data){
-               	result.append('<a href="/user/' +data[key].id + '"> ' + '<li class="list-group-item list-group-item-dark list-group-item-action "  style="height: 40px; width: 300px; padding: 10px 15px;"> <h5>' + data[key].firstName + '  ' + data[key].lastName + '</h5></a></li><br>');
+                result.append(`
+                <a href="/user/${data[key].id}">
+                 <div class="card bg-danger text-white" style="width: 20rem;">
+                 <div class="card-body"><h4 style="height: 8px;">${data[key].firstName} ${data[key].lastName}</h4></div>
+                 </div>
+                 <br>
+                `);
+
                }
-               result.append('</ul>');
-               result.append('</div>');                   
+               result.append('</div>');
+                                 
             },
 
             cache: false,
